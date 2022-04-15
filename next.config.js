44c154bd5d2d80183115af02a8aa24d8b667f556
rizqi <rev4rmy@gmail.com>
2022-04-15 16:46:48 +0700

    (migrate) migrate deployment from gh-page to vercel

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/skipfortoday/nextjs-tailwind-table.github.io/gh-pages/"
-    : "",
+  // assetPrefix: isProd
+  //   ? "https://cdn.statically.io/gh/skipfortoday/nextjs-tailwind-table.github.io/gh-pages/"
+  //   : "",
 };
 
 module.exports = nextConfig;
