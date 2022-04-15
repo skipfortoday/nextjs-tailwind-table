cc2582dc90240610b0d01053d1b747cec578133e
rizqi <rev4rmy@gmail.com>
2022-04-15 16:50:56 +0700

    (fix) deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
-  // assetPrefix: isProd
-  //   ? "https://cdn.statically.io/gh/skipfortoday/nextjs-tailwind-table.github.io/gh-pages/"
-  //   : "",
 };
 
 module.exports = nextConfig;
