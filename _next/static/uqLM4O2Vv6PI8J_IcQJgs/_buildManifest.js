ceb72df526a9dfea256578f614f6d0651eefccc8
skipfortoday <skipfortoday@users.noreply.github.com>
2022-04-15 04:47:06 +0000

    Deploying to gh-pages from @ skipfortoday/nextjs-tailwind-table@3b6a13e014ec78d18901aff0e8f49c71a004b23a 🚀

---
+++ _next/static/uqLM4O2Vv6PI8J_IcQJgs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5208445f694a9c50.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
